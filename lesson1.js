25d42cde7644821fbcb21b3b42152b37d7cb4932
Will Walton <will@endless3dge.com>
2024-01-24 00:07:28 -0600

    Took Notes
    These were notes takes for my son to read over while explaining to him

--- lesson1.js
+++ lesson1.js
@@ -1,1 +1,26 @@
 // JavaScript Document
+let message; // a variable is a storage container, to declare what a variable is use (var, let const)
+// the storage container is 'message'
+message = 'Hello'; // store the strong 'Hello' in the variable named message
+
+alert(message); //shows the variable content 
+
+let messageA = 'HelloBoo'; //define he variable and assign the value in one line of code 
+alert(messageA);
+
+let messageB;
+
+messageB = 'Whats up';
+messageB = 'World'; //value changed 
+
+
+let admin, name;
+
+name = 'John'
+admin = 'name'
+
+alert(admin);
+
+let homePlanetName = 'Earth';
+let currentVisitorName = 'Will';
+
